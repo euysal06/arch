c8732a5bf45ce1304717e0767aac05a6a733d816
euysal06 <64211969+euysal06@users.noreply.github.com>
2020-04-23 18:25:10 +0300

    Add files via upload

---
+++ install2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
+
+hwclock --systohc
+
+sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+
+sed -i 's/#\(tr_TR\.UTF-8\)/\1/' /etc/locale.gen
+
+locale-gen
+
+echo "LANG=tr_TR.UTF-8" > /etc/locale.conf
+
+echo "KEYMAP=trq" > /etc/vconsole.conf
+
+echo "denk" > /etc/hostname
+
+mkinitcpio -P
+
+pacman -S --noconfirm grub os-prober efibootmgr
+
+mkdir /boot/efi
+
+mount /dev/sda2 /boot/efi
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Archlinux
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+exit
